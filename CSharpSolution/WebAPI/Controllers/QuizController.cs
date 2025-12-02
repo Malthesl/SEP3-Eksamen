b0b668be88a806840249ce4963b9497f70b429ee
Malthe Laursen <malthe@mtsl.dk>
2025-12-02 11:48:51 +0100

    Cleanup, Query quizzes

--- CSharpSolution/WebAPI/Controllers/QuizController.cs
+++ CSharpSolution/WebAPI/Controllers/QuizController.cs
@@ -1,7 +1,7 @@
 using ApiContracts;
 using GrpcClient;
 using Microsoft.AspNetCore.Mvc;
-using QuestionDTO = ApiContracts.QuestionDTO;
+using QuizDTO = ApiContracts.QuizDTO;
 
 namespace WebAPI.Controllers;
 
@@ -9,4 +9,36 @@ namespace WebAPI.Controllers;
 [Route("[controller]")]
 public class QuizController(QuizzesService.QuizzesServiceClient quizService) : ControllerBase
 {
+    /**
+     * Querier alle quizzes fra backend
+     */
+    [HttpGet]
+    public ActionResult<QuizQueryDTO> QueryMany([FromQuery] string? query, [FromQuery] string? visibility,
+        [FromQuery] int? creatorId, [FromQuery] int start = 0, [FromQuery] int count = 20)
+    {
+        var req = new QueryQuizzesRequest
+        {
+            SearchQuery = query,
+            ByCreatorId = creatorId ?? -1,
+            Start = start,
+            End = start + count,
+        };
+
+        req.Visibilities.AddRange(visibility.Split(','));
+
+        var res = quizService.QueryQuizzes(req);
+
+        return Ok(new QuizQueryDTO
+        {
+            Start = res.Start,
+            Count = res.Start - res.End,
+            Quizzes = res.Quizzes.Select(quiz => new QuizDTO
+            {
+                Id = quiz.Id,
+                Title = quiz.Title,
+                CreatorId = quiz.CreatorId,
+                Visibility = quiz.Visibility
+            })
+        });
+    }
 }
