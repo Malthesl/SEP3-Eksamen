708ffec17ea6b0d4e1bb1045937a348d4a8c4138
Malthe Laursen <malthe@mtsl.dk>
2025-12-02 11:31:15 +0100

    Proto filer og klienter på WebAPI delen

--- CSharpSolution/WebAPI/Controllers/QuizController.cs
+++ CSharpSolution/WebAPI/Controllers/QuizController.cs
@@ -7,51 +7,6 @@ namespace WebAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class QuizController(QuizService.QuizServiceClient quizService) : ControllerBase
+public class QuizController(QuizzesService.QuizzesServiceClient quizService) : ControllerBase
 {
-    [HttpGet]
-    public async Task<ActionResult<QuestionListDTO>> GetQuestions()
-    {
-        var response = await quizService.GetAllQuestionsAsync(new GetQuestionsRequest());
-        
-        return Ok(new QuestionListDTO
-        {
-            Questions = response.Question.Select(q => new QuestionDTO
-            {
-                Id = q.Id,
-                Question = q.Question,
-                Answer = q.Answer
-            }).ToList()
-        });
-    }
-    
-    [HttpGet("{questionId}")]
-    public async Task<ActionResult<QuestionDTO>> GetQuestion(int questionId)
-    {
-        var response = await quizService.GetQuestionAsync(new GetQuestionRequest { QuestionId = questionId });
-        
-        return Ok(new QuestionDTO
-        {
-            Id = response.Question.Id,
-            Question = response.Question.Question,
-            Answer = response.Question.Answer
-        });
-    }
-    
-    [HttpPost]
-    public async Task<ActionResult> CreateQuestion(CreateQuestionDTO question)
-    {
-        var response = await quizService.CreateQuestionAsync(new CreateQuestionRequest
-        {
-            Question = question.Question,
-            Answer = question.Answer
-        });
-        
-        return Created($"/questions/{response.Question.Id}", new QuestionDTO
-        {
-            Id = response.Question.Id,
-            Question = response.Question.Question,
-            Answer = response.Question.Answer
-        });
-    }
 }
