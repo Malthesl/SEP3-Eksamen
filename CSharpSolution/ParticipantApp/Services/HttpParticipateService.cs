fec339b15edab568dba61d1b49dcf80c2905ce97
Nikolai Balck <116642207+B4lck@users.noreply.github.com>
2025-12-05 09:32:45 +0100

    Rename fix og deltager app

---
+++ CSharpSolution/ParticipantApp/Services/HttpParticipateService.cs
@@ -0,0 +1,28 @@
+﻿using ApiContracts;
+
+namespace ParticipantApp.Services;
+
+public class HttpParticipateService(HttpClient httpClient, IPlayerService playerService) : IParticipateService
+{
+    public async Task<string> JoinAsync(string code, string name)
+    {
+        var res = await httpClient.PostAsJsonAsync("/live/join", new LiveGameJoinRequestDTO()
+        {
+            Name = name,
+            JoinCode = code
+        });
+        
+        if (!res.IsSuccessStatusCode) throw new HttpRequestException(res.ReasonPhrase);
+        
+        var content = (await res.Content.ReadFromJsonAsync<LiveGameJoinResponseDTO>())!;
+        
+        await playerService.Set(content.PlayerId, content.GameId);
+        
+        return content.GameId;
+    }
+
+    public Task<string> AnswerAsync(string gameId, string playerId, int questionId, int answerId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CSharpSolution/ParticipantApp/Services/IPlayerService.cs
@@ -0,0 +1,9 @@
+﻿namespace ParticipantApp.Services;
+
+public interface IPlayerService
+{
+    public Task<string?> GetPlayerIdAsync(); 
+    public Task<string?> GetGameIdAsync(); 
+    public Task Set(string playerId, string gameId);
+    public Task Clear();
+}
